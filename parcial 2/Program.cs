39b1a0693c8b5a115efeaec41e1bcd128f5b3ea2
pvasqv <Laurasofia12%>
2023-09-22 20:26:56 -0500

    prueba

--- parcial 2/Program.cs
+++ parcial 2/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿//Desarrollar el juego "adivina el numero" en un programa en c#, consiste en adivinar un numero N aleatorio//
+//que el sistema escoge de un determinado rango y lo guarda en memoria sin ser revelado//
 using System;
 using System.Runtime.CompilerServices;
 
@@ -8,16 +9,25 @@ namespace ResolucionParcial2
     {
         static void Main(string[] args)
         {
-            //Datos de entrada --> numberPlayers, min = 0, max, numberAttempt 
-            //Datos de proceso --> randomNumber
-            //Datos de salida --> resetGame, hasWinner
-
             int numberPlayers, min = 0, max, numberAttempt = 0, randomNumber = 0;
             bool hasWinner = false, isPlayAgain;
             string resetGame = "";
 
             do
             {
+                PaintMenu(out numberPlayers);
+                randomNumber = GenerateRandomNumber(min, out max, randomNumber, numberPlayers);
+                PlayGame(numberPlayers, randomNumber, numberAttempt, min, max, hasWinner);
+                isPlayAgain = PlayAgain(resetGame);
+            } while (isPlayAgain);
+        }
+        private static bool PlayAgain(string resetGame)
+        {
+            Console.WriteLine("Si quieren jugar de nuevo digita YES o NO para finalizar");
+            resetGame = Console.ReadLine();
+            Console.Clear();
+
+
 
 
 
