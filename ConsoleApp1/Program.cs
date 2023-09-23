bd0a819028454e373e61c0fcbbf1c35ceaac491d
Jorge <Jorge@DESKTOP-MSOVDS0>
2023-09-22 21:07:27 -0500

    Proceso del random de 2 jugadores

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,19 +5,34 @@ public class JuegoParcial2
 {
     static void Main(string[] args)
     {
-        int numJugadores; // declarar variables
+        int numJugadores,num; // declarar variables
 
         Random rand1 = new Random(); // Asignar números aleatorios
         Random rand2 = new Random();
         Random rand3 = new Random();
 
-        rand1.Next(0, 51);
-
         Console.WriteLine("Ingrese el número de jugadores de 2 a 4: ");
+        numJugadores = Convert.ToInt32(Console.ReadLine());
 
         if (numJugadores == 2)
         {
+            rand1.Next(0, 51);
 
+            Console.WriteLine("Ingrese el primer número");
+            num = Convert.ToInt32(Console.ReadLine());
+           
+            if (num > rand1)
+            {
+                Console.WriteLine("El número es menor");
+            } 
+            else if (num < rand1)
+            {
+                Console.WriteLine("El número es mayor");
+            }
+            else
+            {
+                Console.WriteLine("HAS GANADO");
+            }
         }
         else if (numJugadores == 3)
         {
