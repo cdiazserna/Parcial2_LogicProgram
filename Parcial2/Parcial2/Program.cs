2f8a622324451bfcf94355436b3f1749797af188
Gisella Hurtado Mosquera <ghurtadomosquera504@gmail.com>
2023-09-22 20:17:46 -0500

    Declaracion de variables

--- Parcial2/Parcial2/Program.cs
+++ Parcial2/Parcial2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Desarrollar el juego “Adivina el número” en un programa de C#. El juego es muy sencillo, consiste en adivinar  un  número ℕaleatorio  que  el  sistema  escoge  de  un  determinado  rango  y lo guarda  en memoriasin ser revelado
+public class parial2
+{
+    static void Main(string[] args)
+    {
+        int numeroAleatorio;
+        int numeroIngresado;
+        int numeroJugadore;
+        int turno;
+        bool ganador;
+        string respuesta;
+
+        {
+
+            Console.Write("cuantos jugadores van a jugar: ");
+            
+        }
+    }
+}
