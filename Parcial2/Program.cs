2dece8044837bc855b2c111e48010639595b7591
Marlon Foronda <forondamarlon642@gmail.com>
2023-09-22 21:22:41 -0500

    continuación solucion parcial 2

--- Parcial2/Program.cs
+++ Parcial2/Program.cs
@@ -9,12 +9,16 @@ namespace Parcial2
 
             Random random = new Random();
 
-            int NumParticipantes = 0, numGanador;
+            int NumParticipantes = 0, numGanador, jugador1, jugador2, jugador3, jugador4;
             numGanador = random.Next(0, 100);
+            
+            Console.WriteLine("jugador 1 ingrese el número:  ");
+            Convert.ToInt32("Participante 1 ingrese el número:  ");
 
-            Console.WriteLine("Participante 1 ingrese el número:  ");
             Console.WriteLine("Participante 2 ingrese el número:  ");
+
             Console.WriteLine("Participante 3 ingrese el número:  ");
+
             Console.WriteLine("Participante 4 ingrese el número:  ");
 
 
@@ -24,8 +28,11 @@ namespace Parcial2
 
             if (NumParticipantes == 2)
             {
-                numGanador = random.Next(0,50);
-                
+                Console.WriteLine("jugador 1 ingrese el número:  ");
+
+                Console.WriteLine("jugador 2 ingrese el número:  ");
+
+
             }
 
 
@@ -33,7 +40,7 @@ namespace Parcial2
 
             if (NumParticipantes == 3)
             {
-                numGanador = random.Next(0, 100);
+                Console.WriteLine("jugador 3 ingrese el número:  ");
 
             }
 
@@ -42,7 +49,7 @@ namespace Parcial2
 
             if (NumParticipantes == 4)
             {
-                numGanador = random.Next(0, 200);
+                Console.WriteLine("jugador 4 ingrese el número:  ");
 
             }
 
