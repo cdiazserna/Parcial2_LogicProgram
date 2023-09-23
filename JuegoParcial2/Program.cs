41a573ddce18c0ba2678f1f24f720fea8b3eecbb
Juan Manuel Ospina Londoño <juanmasgr@gmail.com>
2023-09-22 21:04:10 -0500

    en este paso estoy generando los condicionales para que el juego escoja el numero aleatorio segun el rango

--- JuegoParcial2/Program.cs
+++ JuegoParcial2/Program.cs
@@ -9,23 +9,63 @@ namespace juegoparcial2
         public static void Main(string[] args)
         {
             int numjugadores, num1;
-            const string MESSAGE = "Por favor ingresar un número de jugadores";
-            
+            const string MESSAGE = "El juego permite maximo 4 jugadores";
 
-            Console.Write("Ingrese un numero entero del 1 al 100 ");
-            if (!int.TryParse(Console.ReadLine(), out num1)) 
-            do
+            Console.Write("Ingrese el numero de jugadores, maximo 4 ");
+            numjugadores = Convert.ToInt32(Console.ReadLine());
+            if (numjugadores == 2)
             {
-                Console.WriteLine(MESSAGE);
-                Console.Clear();
-                Console.Write("Ingrese el primer número: ");
                 Random r = new Random();
 
-         
-            Console.WriteLine(r.Next(1, 101));
 
-            Console.ReadLine();
-        }
+                Console.WriteLine(r.Next(0, 51));
 
+                Console.ReadLine();
+            }
+            if (numjugadores == 3)
+            {
+                Random r = new Random();
+
+
+                Console.WriteLine(r.Next(0, 101));
+
+                Console.ReadLine();
+            }
+            else
+                (numjugadores == 4)
+            {
+                Random r = new Random();
+
+
+                Console.WriteLine(r.Next(0, 201));
+
+                Console.ReadLine();
+
+            }
+
+
+
+                Console.Write("Ingrese un numero entero del 1 al 100 ");
+            if (!int.TryParse(Console.ReadLine(), out num1))
+                
+                {
+                    Console.WriteLine(MESSAGE);
+                    Console.Clear();
+                    Console.Write("Ingrese un nùmero ");
+
+
+
+                    Random r = new Random();
+
+
+                    Console.WriteLine(r.Next(1, 101));
+
+                    Console.ReadLine();
+
+
+
+                }
+            }
     }
 }
+
