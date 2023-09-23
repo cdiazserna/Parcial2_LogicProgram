bf1c39400357971dd718e20dc24f0d998bf76c6d
SantiagoFU - BK <santiagoflorez287345@correo.itm.edu.co>
2023-09-22 20:05:48 -0500

    Se declaran variables

---
+++ Parcial2/Parcial2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Parcial2;
+
+public class Punto1Parcial2
+
+{
+    static void Main(string[] args)
+
+
+    {
+        Random random = new Random();
+
+
+        //Declaro variables
+        int cantidadParticipantes, numeroAleatorio;
+        bool repetir = true;
+
+
+
+        numeroAleatorio = random.Next(0, 100);
+
+    }
+}
+        
