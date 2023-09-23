8f15a517cebbe535cd9b5760a436ff4395deec40
Jorge <Jorge@DESKTOP-MSOVDS0>
2023-09-22 20:57:14 -0500

    asignacion de números aleatorios

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,34 @@
 ﻿
+using System.ComponentModel.Design;
+
 public class JuegoParcial2
 {
     static void Main(string[] args)
     {
         int numJugadores; // declarar variables
+
+        Random rand1 = new Random(); // Asignar números aleatorios
+        Random rand2 = new Random();
+        Random rand3 = new Random();
+
+        rand1.Next(0, 51);
+
+        Console.WriteLine("Ingrese el número de jugadores de 2 a 4: ");
+
+        if (numJugadores == 2)
+        {
+
+        }
+        else if (numJugadores == 3)
+        {
+            rand2.Next(0, 100);
+        }
+        else if (numJugadores == 4)
+        {
+            rand3.Next(0, 200);
+        }
     }
+       
+            
+
+        
