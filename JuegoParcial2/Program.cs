e2464794528249455883960c8acff16cdb5c8d31
Sebastián Zerpa Vanegas <sebastianzerpav@outlook.com>
2023-09-22 20:26:58 -0500

    random clase

--- JuegoParcial2/Program.cs
+++ JuegoParcial2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class JuegoParcial2
+{
+    static void Main(string[] args)
+    {
+        Random numOculto = new Random();
+        int numJugadores, num_Oculto;
+
+        Console.WriteLine("¡Bienvenido al juego: Adivina el número!\n");
+        Console.WriteLine("¿Cuántos jugadores van a participar? (Min: 2; Max: 4)");
+        numJugadores = int.Parse(Console.ReadLine());
+
+        if (numJugadores == 2)
+        {
+            num_Oculto = numOculto.Next(0, 50);
+        } else if(numJugadores == 3){
+
+        }
+        else
+        {
+
+        }
+
+
+    }
+}
