747204f351980b572a738fd5d03bf32ee9ad1b1b
Jonathan Garcia <jonatan.61191@hotmail.com>
2023-09-22 21:24:35 -0500

    prueba al codigo

--- parcial2 Jonathan Garcia/Program.cs
+++ parcial2 Jonathan Garcia/Program.cs
@@ -14,89 +14,117 @@ namespace Parcial2
         {
             //Declaracion de variables
             int numeroInte, numN=0, numIngresado;
-            
+            string nuevoTiro;
+            bool newCod = false;
+
+
+
             //Generar numero aleatorio
 
-            Console.WriteLine("BIENVENIDO AL JUEGO");
-            Console.WriteLine("\n");
-            Console.Write("Por favor ingresar el numero de integrantes que desea participar: ");
-            numeroInte = Convert.ToInt32(Console.ReadLine());
-            Random Random = new Random();
-            if (numeroInte < 2) 
+            do
             {
-                Console.WriteLine("El numero de integrantes debe ser mayor a 2 y menor a 4");
+                Console.WriteLine("BIENVENIDO AL JUEGO");
                 Console.WriteLine("\n");
-                Console.WriteLine("Por favor ingresar el numero de integrantes que desea participar: ");
+                Console.Write("Por favor ingresar el numero de integrantes que desea participar: ");
                 numeroInte = Convert.ToInt32(Console.ReadLine());
-            }
-            else 
-            {
-                if (numeroInte > 4)
+                Random Random = new Random();
+                if (numeroInte < 2)
                 {
-                    Console.WriteLine("El numero de integrantes debe ser mayor o igual a 2 y menor o igual a 4");
+                    Console.WriteLine("El numero de integrantes debe ser mayor a 2 y menor a 4");
                     Console.WriteLine("\n");
                     Console.WriteLine("Por favor ingresar el numero de integrantes que desea participar: ");
                     numeroInte = Convert.ToInt32(Console.ReadLine());
-
-                                 
                 }
-            }
+                else
+                {
+                    if (numeroInte > 4)
+                    {
+                        Console.WriteLine("El numero de integrantes debe ser mayor o igual a 2 y menor o igual a 4");
+                        Console.WriteLine("\n");
+                        Console.WriteLine("Por favor ingresar el numero de integrantes que desea participar: ");
+                        numeroInte = Convert.ToInt32(Console.ReadLine());
 
-            if (numeroInte == 2)
-            { 
-                numN = Random.Next(0, 51); 
-            }
 
-            else
-            {
-                if (numeroInte == 3)
-                { numN = Random.Next(0, 100); }
+                    }
+                }
+
+                if (numeroInte == 2)
+                {
+                    numN = Random.Next(0, 51);
+                }
+
+                else
+                {
+                    if (numeroInte == 3)
+                    { numN = Random.Next(0, 100); }
 
                     else
                     {
-                    if (numeroInte == 4)
-                        numN = Random.Next(0, 200);
+                        if (numeroInte == 4)
+                            numN = Random.Next(0, 200);
                     }
-                
-            }
-            //Iniciar el ciclo de adivinar e4l numero
-            do
-            {
-                Console.WriteLine("\n");
-                Console.WriteLine("Ingrese el numero que cree que es el correcto:");
-                Console.WriteLine("\n");
-                numIngresado = Convert.ToInt32(Console.ReadLine());
 
-                if (numIngresado > numN)
-                {
-                    Console.WriteLine("\n");
-                    Console.WriteLine("MENOR");
-                    Console.WriteLine("\n");
                 }
-
-                else 
+                //Iniciar el ciclo de adivinar el numero
+                do
                 {
-                    if (numIngresado < numN)
+                    Console.WriteLine("\n");
+                    Console.WriteLine("Ingrese el numero que cree que es el correcto:");
+                    Console.WriteLine("\n");
+                    numIngresado = Convert.ToInt32(Console.ReadLine());
+
+                    if (numIngresado > numN)
                     {
                         Console.WriteLine("\n");
-                        Console.WriteLine("MAYOR");
+                        Console.WriteLine("MENOR");
                         Console.WriteLine("\n");
                     }
-                    else  
+
+                    else
                     {
-                        if (numIngresado == numN)
+                        if (numIngresado < numN)
                         {
                             Console.WriteLine("\n");
-                            Console.WriteLine("¡HAS GANADO!”. Aquí ya finaliza el juego.");
+                            Console.WriteLine("MAYOR");
                             Console.WriteLine("\n");
                         }
+                        else
+                        {
+                            if (numIngresado == numN)
+                            {
+                                Console.WriteLine("\n");
+                                Console.WriteLine("¡HAS GANADO!”. Aquí ya finaliza el juego.");
+                                Console.WriteLine("\n");
+                            }
 
+                        }
                     }
+
                 }
+
+                while (numIngresado != numN);
+
+
+                Console.WriteLine("\n");
+                Console.WriteLine("¿¿¿ Desea  jugar un nuevo “tirito” ??? ");
+                Console.WriteLine("¿¿¿ Ingrese SI o NO ??? ");
+                Console.WriteLine("\n");
+                nuevoTiro = Convert.ToString(Console.ReadLine());
+
+                if (nuevoTiro == "SI")
+                {
+                    newCod = true;
+
+                }
+            }
+            while (true);
+
+    {
 
             }
 
-            while (numIngresado != numN);
+
+
 
         }
 
