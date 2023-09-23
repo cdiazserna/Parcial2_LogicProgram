6bb5bcc4a7751c23283f111c6d27a62d8dd82ab4
Gisella Hurtado Mosquera <ghurtadomosquera504@gmail.com>
2023-09-22 20:56:59 -0500

    Processo para crear el juego, realice unos cambios

--- Parcial2/Parcial2/Program.cs
+++ Parcial2/Parcial2/Program.cs
@@ -1,4 +1,6 @@
 ﻿//Desarrollar el juego “Adivina el número” en un programa de C#. El juego es muy sencillo, consiste en adivinar  un  número ℕaleatorio  que  el  sistema  escoge  de  un  determinado  rango  y lo guarda  en memoriasin ser revelado
+using System;
+
 public class parial2
 {
     static void Main(string[] args)
@@ -10,8 +12,11 @@ public class parial2
         bool ganador;
         string respuesta;
 
-        
+        Random random = new Random();
+
+
         {
+            Console.WriteLine("BIENVENIDO AL JUEGO: ");
 
             Console.WriteLine("cuantos jugadores van a jugar  (Mínimo 2 y máximo 4)\");\n");
 
@@ -23,21 +28,33 @@ public class parial2
                 {
                     Console.WriteLine("El número de jugadores debe ser entre 2 y 4. Inténtalo de nuevo.");
                     numeroJugadores = Convert.ToInt32 (Console.ReadLine());
+                {
+                        numeroAleatorio = random.Next(0, 51); // Entre 0 y 50
+                        break;
+                   
+                        numeroAleatorio = random.Next(0, 101); // Entre 0 y 100
+                        break;
+              
+                        numeroAleatorio = random.Next(0, 201); // Entre 0 y 200
+                        break;
+        
+                        numeroAleatorio = 0; // Valor por defecto
+                        break;
 
-                    
+
+                    }
+
+
+
+
+
+
+
+
+
+
+
+
                 }
-
-
-
-
-
-
-
-
-
-
-
-
-        }
     }
 }
