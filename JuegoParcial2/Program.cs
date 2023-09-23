75e5880dcb8848a544ae03a4dc336f769e7bd6e7
Xkals <141454035+Xkals@users.noreply.github.com>
2023-09-22 20:15:02 -0500

    Declaracion de variables, Se declara la variable incognita y el jugador.

---
+++ JuegoParcial2/Program.cs
@@ -0,0 +1,50 @@
+﻿/* 
+/* El sistema escoge aleatoriamente un número entre 0 y 100, ese número NO SE PUEDE REVELAR ya que es 
+el número que se adivinará, por ende, dicho número se almacenará en memoria. Los participantes comenzarán 
+en orden a
+
+Condiciones de Juego: 
+- Si participan 2 jugadores, el número ℕaleatorio se generará entre 0 y 50-Si participan 3 jugadores, 
+- el númeroℕaleatorio se generará entre 0 y 100-Si participan 4 jugadores, 
+- el númeroℕaleatorio se generará entre 0 y 200
+
+Cada jugador comenzará su turno intentando adivinar ese númeroℕaleatorio.
+El programa deberá mostrarle al jugador la siguiente información:
+-Si  el  número  ingresado  es  mayor  al  número  aleatorio,  
+entonces  mostrar  por  pantalla  la palabra “MENOR” y darle la oportunidad al siguiente jugador.
+-Si  el  número  ingresado  es menor al  número  aleatorio,  
+entonces  mostrar  por  pantalla  la palabra “MAYOR” y darle la oportunidad al siguiente jugador.
+-Si  el  número  ingresado  coincide  con  el  número  aleatorio, 
+entonces  mostrar  por pantalla  la palabra “¡HAS GANADO!”. Aquí ya finaliza el juego.
+
+*/
+
+class Parcial2
+{
+    static void Main()
+
+    { 
+    Random N = new Random();
+        
+        int numeroIngresado,;
+
+        namespace AdivinaElNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            int incognita = random.Next(0, 100); // Genera un número aleatorio entre 0 y 100
+            int jugador = 0;
+
+          
+        }
+    }
+}
+    
+    
+    
+    
+    }
+}
