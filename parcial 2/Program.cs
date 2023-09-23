3c3c1b179c104a279629869d5478815e8403d9dd
pvasqv <Laurasofia12%>
2023-09-22 20:11:56 -0500

    prueba

--- parcial 2/Program.cs
+++ parcial 2/Program.cs
@@ -1,1 +1,24 @@
 ﻿
+using System;
+using System.Runtime.CompilerServices;
+
+namespace ResolucionParcial2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Datos de entrada --> numberPlayers, min = 0, max, numberAttempt 
+            //Datos de proceso --> randomNumber
+            //Datos de salida --> resetGame, hasWinner
+
+            int numberPlayers, min = 0, max, numberAttempt = 0, randomNumber = 0;
+            bool hasWinner = false, isPlayAgain;
+            string resetGame = "";
+
+            do
+            {
+
+
+
+
