5aaf9d22c434f62e5c46a635752d994719c66fa6
SANTIAGOGALVIS04 <sgalvis1904@gmail.com>
2023-09-22 20:36:50 -0500

    Iniciamos el proceso. Pedimos el número de jugadores a los usuarios y hacemos el cambio de nombre a las variables para que sea más claro

--- JuegoParcial2/JuegoParcial2/Program.cs
+++ JuegoParcial2/JuegoParcial2/Program.cs
@@ -11,7 +11,26 @@ namespace JuegoParcial2
 
             Random numeroRandom = new Random();
 
-            int jugadores, numeroJugador;
+            int numeroJugadores, numeroEscogido;
+
+            //Proceso del juego
+
+            //Pedimos el número de jugadores
+
+            Console.WriteLine("Bienvenidos al juego de adivinar el número");
+            Console.WriteLine("Ingrese el número de jugadores que desean jugar\n" +
+                "mínimo deben de ser 2 jugadores y máximo 4");
+
+            numeroJugadores = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 2;i <= numeroJugadores; i++)
+            {
+
+                
+
+            }
+            
 
 
         }
