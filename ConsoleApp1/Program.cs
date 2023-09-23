3b2c50c5b27c4968500b1b261da968b9a83fb086
Jorge <Jorge@DESKTOP-MSOVDS0>
2023-09-22 21:23:46 -0500

    generar el nuevo intento

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,8 @@ public class JuegoParcial2
     static void Main(string[] args)
     {
         int numJugadores,num; // declarar variables
+        bool continuar = true;
+        string respuesta;
 
         Random rand1 = new Random(); // Asignar números aleatorios
         Random rand2 = new Random();
@@ -14,66 +16,74 @@ public class JuegoParcial2
         Console.WriteLine("Ingrese el número de jugadores de 2 a 4: ");
         numJugadores = Convert.ToInt32(Console.ReadLine());
 
-
-        while (numJugadores == 2)
+        while (continuar)
         {
-            rand1.Next(0, 51);
+            while (numJugadores == 2)
+            {
+                rand1.Next(0, 51);
 
-            Console.WriteLine("Ingrese un número");
-            num = Convert.ToInt32(Console.ReadLine());
-           
-            if (num > rand1)
-            {
-                Console.WriteLine("El número es menor");
-            } 
-            else if (num < rand1)
-            {
-                Console.WriteLine("El número es mayor");
-            }
-            else
-            {
-                Console.WriteLine("HAS GANADO");
-            }
-        }
-        while (numJugadores == 3)
-        {
-            rand2.Next(0, 100);
+                Console.WriteLine("Ingrese un número");
+                num = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Ingrese un número");
-            num = Convert.ToInt32(Console.ReadLine());
+                if (num > rand1)
+                {
+                    Console.WriteLine("El número es menor");
+                }
+                else if (num < rand1)
+                {
+                    Console.WriteLine("El número es mayor");
+                }
+                else
+                {
+                    Console.WriteLine("HAS GANADO");
+                }
+            }
+            while (numJugadores == 3)
+            {
+                rand2.Next(0, 100);
 
-            if (num > rand2)
-            {
-                Console.WriteLine("El número es menor");
-            }
-            else if (num < rand2)
-            {
-                Console.WriteLine("El número es mayor");
-            }
-            else
-            {
-                Console.WriteLine("HAS GANADO");
-            }
-        }
-        while (numJugadores == 4)
-        {
-            rand3.Next(0, 200);
+                Console.WriteLine("Ingrese un número");
+                num = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Ingrese un número");
-            num = Convert.ToInt32(Console.ReadLine());
+                if (num > rand2)
+                {
+                    Console.WriteLine("El número es menor");
+                }
+                else if (num < rand2)
+                {
+                    Console.WriteLine("El número es mayor");
+                }
+                else
+                {
+                    Console.WriteLine("HAS GANADO");
+                }
+            }
+            while (numJugadores == 4)
+            {
+                rand3.Next(0, 200);
 
-            if (num > rand3)
-            {
-                Console.WriteLine("El número es menor");
+                Console.WriteLine("Ingrese un número");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (num > rand3)
+                {
+                    Console.WriteLine("El número es menor");
+                }
+                else if (num < rand3)
+                {
+                    Console.WriteLine("El número es mayor");
+                }
+                else
+                {
+                    Console.WriteLine("HAS GANADO");
+                }
             }
-            else if (num < rand3)
-            {
-                Console.WriteLine("El número es mayor");
-            }
-            else
-            {
-                Console.WriteLine("HAS GANADO");
-            }
+            Console.WriteLine(String.Format("Desea jugar de nuevo? (s/n)");
+            respuesta = Console.ReadLine();
+
+            if (!respuesta = "s") ;
+
+            continuar = false;
         }
     }
        
