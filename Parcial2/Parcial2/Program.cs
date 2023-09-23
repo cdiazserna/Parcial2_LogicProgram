fe6b53c22b5b15b41555fbe3880d57db75fb1c52
SantiagoOspinaR <ospinasantiago41@gmail.com>
2023-09-22 21:24:03 -0500

    C

--- Parcial2/Parcial2/Program.cs
+++ Parcial2/Parcial2/Program.cs
@@ -8,22 +8,46 @@ public class JuegoParcial2
     static void Main(string[] args)
         
 {
+        Random numeroGenerado = new Random();
         //Declaracion de las variables
       int numeroJugadores, numeroGanador;
       bool proximoIntento = true;
 
         Console.WriteLine("----------ADIVINA EL NÚMERO----------\n");
-        Console.WriteLine("Por favor Ingrese el número de jugadores que van a participar en el juego, teniendo en cuenta que solo podrán jugar entre 2 y 4 jugadores: ");
+        Console.WriteLine("Por favor Ingrese el número de jugadores que van a participar en el juego. Tenga en cuenta que solo podrán jugar entre 2 y 4 jugadores: ");
         numeroJugadores = Convert.ToInt32(Console.ReadLine());
-        
-        if (numeroJugadores < 2);
+
+        for (int i = 0; i <= 200; i++)
+
+        if (numeroJugadores < 2 && numeroJugadores > 4 );
         {
-            Console.WriteLine("El juego no puede comenzar si no hay al menos 2 jugadores");
+            Console.WriteLine("Porfavor, ingresar un número de jugadores en el rango indicado");
         }
-        if (numeroJugadores > 4);
+        if (numeroJugadores == 2)
         {
-         Console.WriteLine("Porfavor, ingresar un número de jugadores en el rango indicado");
+            numeroGenerado.Next(0, 50);
+            Console.WriteLine("Jugador 1: Ingrese su número:");
+            Console.WriteLine("Jugador 2: Ingrese su número: ");
         }
+        else if (numeroJugadores == 3)
+        {
+            numeroGenerado.Next(0, 100);
+            Console.WriteLine("Jugador 1: Ingrese su número:");
+            Console.WriteLine("Jugador 2: Ingrese su número: ");
+            Console.WriteLine("Jugador 3: Ingrese su número: ");
+        }
+        else if (numeroJugadores == 4)
+        {
+           numeroGenerado.Next(0, 200);
+            Console.WriteLine("Jugador 1: Ingrese su número:");
+            Console.WriteLine("Jugador 2: Ingrese su número: ");
+            Console.WriteLine("Jugador 3: Ingrese su número: ");
+            Console.WriteLine("Jugador 4: Ingrese su número: ");
+        }
+
+
+
+
 
 
 
