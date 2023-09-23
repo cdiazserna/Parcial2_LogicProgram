db0cb109d0d6b587b295fdcfdbeb4060383f0ce0
santi04 <santivelas5@gmail.com>
2023-09-22 20:48:54 -0500

    Avance 2

--- JuegoParcial2/JuegoParcial2/Program.cs
+++ JuegoParcial2/JuegoParcial2/Program.cs
@@ -3,7 +3,7 @@
     static void Main(string[] args)
     {
         //declaracion variables
-        int cantidadJugadores, rango1, rango2, rango3, i ;
+        int cantidadJugadores, rango1, rango2, rango3, i ,numeroJugador;
         bool salir = true;
         Random random = new Random();
         cantidadJugadores = random.Next(2, 4);
@@ -14,23 +14,54 @@
         //proceso
         while(salir)
         {
+            Console.WriteLine("Adivina el número”");
+            Console.Clear();
             for(i=1; i<=cantidadJugadores; i++)
             {
                 if(cantidadJugadores == 2)
                 {
+                    Console.WriteLine("Jugador " + i + "adivine el número: ");
+                    numeroJugador = Convert.ToInt32(Console.ReadLine());
 
+                    if (numeroJugador == rango1)
+                    {
+                        Console.WriteLine("Ganaste papu");
+                        salir = false;
+                    }
+                    else if (numeroJugador < rango1) Console.WriteLine("El número que escogiste es menor");
+                    else Console.WriteLine("El número que escogiste es mayor");
 
                 }
                 else if (cantidadJugadores == 3)
                 {
+                    Console.WriteLine("Jugador " + i + "adivine el número: ");
+                    numeroJugador = Convert.ToInt32(Console.ReadLine());
 
+                    if (numeroJugador == rango2)
+                    {
+                        Console.WriteLine("Ganaste papu");
+                        salir = false;
+                    }
+                    else if (numeroJugador < rango2) Console.WriteLine("El número que escogiste es menor");
+                    else Console.WriteLine("El número que escogiste es mayor");
                 }
                 else
                 {
+                    Console.WriteLine("Jugador " + i + "adivine el número: ");
+                    numeroJugador = Convert.ToInt32(Console.ReadLine());
 
+                    if (numeroJugador == rango3)
+                    {
+                        Console.WriteLine("Ganaste papu");
+                        salir = false;
+                    }
+                    else if (numeroJugador < rango2) Console.WriteLine("El número que escogiste es menor");
+                    else Console.WriteLine("El número que escogiste es mayor");
                 }
                     
             }
+
+            Console.WriteLine("");
 
         }
 
