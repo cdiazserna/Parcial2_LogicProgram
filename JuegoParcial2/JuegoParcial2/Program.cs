0271d5d1f5b3a0db4aaa06dc1b25c933da9745ce
santi04 <santivelas5@gmail.com>
2023-09-22 20:26:13 -0500

    Esqueleto juego

--- JuegoParcial2/JuegoParcial2/Program.cs
+++ JuegoParcial2/JuegoParcial2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class juegoParcial2
+{
+    static void Main(string[] args)
+    {
+        //declaracion variables
+        int cantidadJugadores,rango1, rango2, rango3;
+        bool salir = true;
+        Random random = new Random();
+        cantidadJugadores = random.Next(2, 4);
+        rango1 = random.Next(0, 50);
+        rango2 = random.Next(0, 100);
+        rango3 = random.Next(0, 200);
+
+        //proceso
+        while(salir)
+        {
+
+
+        }
+
+
+
+    }
+
+}
