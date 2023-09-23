e540ff88c41f92bad6f3af00d7aadbd31eb77717
Marlon Foronda <forondamarlon642@gmail.com>
2023-09-22 20:50:48 -0500

    proceso solucion parcial 2

--- Parcial2/Program.cs
+++ Parcial2/Program.cs
@@ -7,14 +7,25 @@ namespace Parcial2
         static void Main(string[] args)
         {// declaracion de mis variables
 
-            int NumParticipantes, numGanador, Random;
+            int NumParticipantes, numGanador;
 
+            if (NumParticipantes < 2 )
+            {
+                else Console.WriteLine()
+
+            }
 
             Console.Write("Participante #1 ingrese el número:  ");
+            Console.Write("Participante #2 ingrese el número:  ");
+            Console.Write("Participante #3 ingrese el número:  ");
+            Console.Write("Participante #4 ingrese el número:  ");
+            // Cálculos (procesos)
+
+            
 
 
 
-        
+
         }
     }
 }
