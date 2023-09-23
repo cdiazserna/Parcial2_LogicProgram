eb74a73c1b2f28bafcdb3ac28c8b450b111695ba
DubierArismendy <p73306275@gmail.com>
2023-09-22 21:21:12 -0500

    ya casi

--- JuegoParcial2/JuegoParcial2/Program.cs
+++ JuegoParcial2/JuegoParcial2/Program.cs
@@ -8,47 +8,56 @@ namespace JuegoParcial2
         {
             //declaración de variables
 
-            double  intento, participantes;
+            int intento, participantes, aleatorio;
             bool iniciar = true;
             bool empezar = true;
-            
+
             Random numero = new Random();
 
             Console.WriteLine("______JUEGO ADIVINA EL NÚMERO_______");
 
-            Console.WriteLine("Escribe la cantidad de participantes: ");
-            participantes = Convert.ToInt32(Console.ReadLine());
-
-            if (participantes < 2 ) ;
-            {
-                Console.WriteLine("Cantidad de participantes incorrecta, mín.2 y máx. 4");
-
-                empezar = false;
-            }
-            else
-            {
-             Console.WriteLine("Cantidad de participantes incorrecta, mín.2 y máx. 4");
-             empezar = false;
-            }
-
             while (empezar == false)
             {
-                Console.WriteLine("ULTIMA OPORTUNIDAD");
+
                 Console.WriteLine("Escribe la cantidad de participantes: ");
                 participantes = Convert.ToInt32(Console.ReadLine());
 
-                if (participantes < 1 ^ participantes > 4) ;
-                {
-                    Console.WriteLine("Cantidad de participantes incorrecta, mín.1 y máx. 4");
+                if (participantes > 5) 
+                { 
 
-  
+               Console.WriteLine("Cantidad de participantes incorrecta, mín.2 y máx. 4");
+
+                    empezar = false;
+                }
+                if (participantes == 2)
+                {
+                    aleatorio = numero.Next(0, 50);
+                }
+                if (participantes == 3)
+                {
+                    aleatorio = numero.Next(0, 100);
+                }
+                if (participantes == 4)
+                {
 
                 }
-                
-                
             }
-            //while () ;
-               {
+
+            
+           
+            
+
+            while (iniciar == true)
+
+            Console.WriteLine("Escribe el número correcto");
+            intento = Convert.ToInt32(Console.ReadLine());
+
+            if (numero == intento)
+
+
+
+
+            {
 
 
             }
