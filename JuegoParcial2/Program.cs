d16d79c00ec86c632d9a9c34a8a413a06f155a2b
sebasredes19 <sebastianespinosavalencia@hotmail.com>
2023-09-22 20:21:49 -0500

    ingreso numero de jugadores

--- JuegoParcial2/Program.cs
+++ JuegoParcial2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Juego:Desarrollar el juego “Adivina el número” en un programa de C#. 
+  El juego es muy sencillo, consiste en adivinar
+  un  número ℕaleatorio  que  el  sistema  escoge  de  un  determinado  
+  rango  y lo guarda  en memoriasin ser revelado.*/
+
+
+public class Ejercicio16
+{
+
+    static void Main(string[] args)
+
+    {
+        Console.WriteLine("Ingresa el numero de jugadores: ");
+       
+    }
+
+}
