1214d2d063d11b4bad40eb3e9e92d79a5b788569
SANTIAGOGALVIS04 <sgalvis1904@gmail.com>
2023-09-22 20:26:39 -0500

    Realizamos la declaración de la variable y utilizamos la clase Random para que la maquina genere el número

---
+++ JuegoParcial2/JuegoParcial2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JuegoParcial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creación de un juego
+            // declaración de variables
+
+            Random numeroRandom = new Random();
+
+            int jugadores, numeroJugador;
+
+
+        }
+    }
+}
