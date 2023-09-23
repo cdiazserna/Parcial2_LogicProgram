3f47ec221c8b676ccce404aebc957e1d932d194e
Marlon Foronda <forondamarlon642@gmail.com>
2023-09-22 20:40:58 -0500

    parcial 2 priemro comit

---
+++ Parcial2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Parcial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// declaracion de mis variables
+
+            int NumParticipantes, numGanador, Random;
+
+
+            Console.Write("Participante #1 ingrese el número:  ");
+
+
+
+        
+        }
+    }
+}
