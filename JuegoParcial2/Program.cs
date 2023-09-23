9edefef0d221762165dab178de25da123d93c07a
sebasredes19 <sebastianespinosavalencia@hotmail.com>
2023-09-22 21:28:35 -0500

    proceso final

--- JuegoParcial2/Program.cs
+++ JuegoParcial2/Program.cs
@@ -16,7 +16,7 @@ public class JuegoParcial2
 
         int cantidadJugadores = ObtenerCantidadJugadores();
 
-        int rangoMaximo = CalcularRangoMaximo(cantidadJugadores);
+        /*int rangoMaximo = CalcularRangoMaximo(cantidadJugadores);*/
         int numeroAdivinar = GenerarNumeroAleatorio(rangoMaximo);
 
         int turno = 1;
@@ -24,6 +24,98 @@ public class JuegoParcial2
         bool Ganaste = false;
 
         while (!Ganaste)
-        
+        {
+            Console.WriteLine($"\nJugador {jugadorActual}, es tu turno.");
+            Console.Write("Elige un numero entre 0 y 50: ");
+            int suposicion = Convert.ToInt32(Console.ReadLine());
+
+            if (suposicion == numeroAdivinar)
+            {
+                Console.WriteLine($"Jugador {jugadorActual}, ganaste");
+                Ganaste = true;
+            }
+            else if (suposicion <= numeroAdivinar)
+            {
+                Console.WriteLine("Ingresa un numero mayor");
+            }
+            else
+            {
+                Console.WriteLine("Ingresa un numero menor");
+            }
+
+            jugadorActual = (jugadorActual % cantidadJugadores) + 1;
+            turno++;
+
+            if (!Ganaste && turno % cantidadJugadores == 0)
+            {
+                Console.WriteLine("El numero correcto es"  + numeroAdivinar);
+                Console.Write("siguiente ronda, comenzar? (S/N): ");
+                string respuesta = Console.ReadLine();
+
+                if (respuesta.ToUpper() != "S")
+                {
+                    break;
+                }
+                else
+                {
+                    numeroAdivinar = GenerarNumeroAleatorio(rangoMaximo);
+                    turno = 0;
+                }
+            }
+        }
+
+        Console.WriteLine("Se acabo el juego");
+    }
+
+    static int ObtenerCantidadJugadores()
+    {
+        int cantidadJugadores;
+        while (true)
+        {
+            if (int.TryParse(Console.ReadLine(), out cantidadJugadores) && cantidadJugadores >= 2 && cantidadJugadores <= 4)
+            {
+                return cantidadJugadores;
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingrese un número válido de jugadores entre 2 y 4.");
+            }
+
+            if (cantidadJugadores == 2)
+            {
+                Console.WriteLine(" Elige un numero entre 0 y 50");
+            }
+            else if (cantidadJugadores == 3)
+            {
+                Console.WriteLine(" Elige un numero entre 0 y 100");
+            }
+
+            else if (cantidadJugadores == 3)
+            {
+                Console.WriteLine(" Elige un numero entre 0 y 50");
+            }
+        }
+    }
+
+   /* static int CalcularRangoMaximo(int cantidadJugadores)
+    {
+        if (cantidadJugadores == 2)
+        {
+            return 50;
+        }
+        else if (cantidadJugadores == 3)
+        {
+            return 100;
+        }
+        else
+        {
+            return 200;
+        }
+    }
+    */
+    static int GenerarNumeroAleatorio(int rangoMaximo)
+    {
+        Random random = new Random();
+        return random.Next(1, rangoMaximo);
     }
 }
