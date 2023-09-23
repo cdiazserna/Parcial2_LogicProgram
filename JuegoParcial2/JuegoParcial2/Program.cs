b6912e6416c5e59aa17768c3fd934826d69c9f34
DubierArismendy <p73306275@gmail.com>
2023-09-22 21:02:13 -0500

    fg

--- JuegoParcial2/JuegoParcial2/Program.cs
+++ JuegoParcial2/JuegoParcial2/Program.cs
@@ -19,22 +19,19 @@ namespace JuegoParcial2
             Console.WriteLine("Escribe la cantidad de participantes: ");
             participantes = Convert.ToInt32(Console.ReadLine());
 
-            if (participantes < 1 ^ participantes > 4) ;
+            if (participantes < 2 ) ;
             {
-                Console.WriteLine("Cantidad de participantes incorrecta, mín.1 y máx. 4");
+                Console.WriteLine("Cantidad de participantes incorrecta, mín.2 y máx. 4");
 
                 empezar = false;
-
             }
-            if (participantes == 1) ;
-
-            numero = numero.Next()
+            else
             {
-
+             Console.WriteLine("Cantidad de participantes incorrecta, mín.2 y máx. 4");
+             empezar = false;
             }
 
-
-            while (!empezar) ;
+            while (empezar == false)
             {
                 Console.WriteLine("ULTIMA OPORTUNIDAD");
                 Console.WriteLine("Escribe la cantidad de participantes: ");
