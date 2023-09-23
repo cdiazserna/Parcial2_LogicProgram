f3d5300ba103c10986e11d82a241990ac6efd80e
omora79 <oamm30@hotmail.com>
2023-09-22 20:10:05 -0500

    Declaracion de variables

---
+++ Parcial2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Parcial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //definir las varibles
+
+            int numJugadores, player1, player2, player3, player4, i, n;
+        }
+    }
+}
