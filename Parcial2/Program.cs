7b648a0940105e5da4aefcac41562a9010546fb8
omora79 <oamm30@hotmail.com>
2023-09-22 20:53:49 -0500

    tercer

--- Parcial2/Program.cs
+++ Parcial2/Program.cs
@@ -20,7 +20,7 @@ namespace Parcial2
             if (numJugadores ==2)
             {
                 n = numrandom.Next(0, 50);
-                for (i = 0, i = 50; i++)
+                for (i = 0; i <= 50; i++ )
                 {
                     Console.WriteLine("primer jugador ingresa tu numero entre 0 y 50:  ");
                     player1 = Convert.ToInt32(Console.ReadLine());
@@ -33,12 +33,64 @@ namespace Parcial2
                     }
                     else if (player1 < n)
                     {
-                        Console.WriteLine("el numero es mayor");
+                        Console.WriteLine("el numero del primer jugador es mayor");
                     }
                     else
                     {
-                        Console.WriteLine("El numero es menor");
+                        Console.WriteLine("El numero del primer jugador es  menor");
                     }
+                    if (player2 == n)
+                    {
+                        Console.WriteLine("el segundo jugador a ganado");
+                    }
+                    else if (player2 < n)
+                    {
+                        Console.WriteLine("el numero del segundo jugador es mayor");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero del segundo jugador es  menor");
+                    }
+                }
+
+
+                }
+            else if (numJugadores ==3)
+            {
+                n = numrandom.Next(0, 100);
+                for (i = 0; i <= 50; i++)
+                {
+                    Console.WriteLine("primer jugador ingresa tu numero entre 0 y 100:  ");
+                    player1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("segundo jugador ingresa tu numero entre 0 y 100:  ");
+                    player2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("tercer jugador ingresa tu numero entre 0 y 100:  ");
+                    player3 = Convert.ToInt32(Console.ReadLine());
+
+                    if (player1 == n)
+                    {
+                        Console.WriteLine("el primer jugador a ganado");
+
+                    }
+                    else if (player1 < n)
+                    {
+                        Console.WriteLine("el numero del primer jugador es mayor");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero del primer jugador es  menor");
+                    }
+                    if (player2 == n)
+                    {
+                        Console.WriteLine("el segundo jugador a ganado");
+                    }
+                    else if (player2 < n)
+                    {
+                        Console.WriteLine("el numero del segundo jugador es mayor");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero del segundo jugador es  menor");
                     }
                 }
                 
@@ -47,4 +99,4 @@ namespace Parcial2
 
         }
     }
-}
+
