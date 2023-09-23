ff6041577df1544b74ec9d4825316ed4d380b2bc
omora79 <oamm30@hotmail.com>
2023-09-22 20:16:21 -0500

    toma de datos

--- Parcial2/Program.cs
+++ Parcial2/Program.cs
@@ -9,6 +9,19 @@ namespace Parcial2
             //definir las varibles
 
             int numJugadores, player1, player2, player3, player4, i, n;
+            Random numrandom = new Random();
+
+            // pedir datos a los participantes.
+
+            Console.WriteLine("------------Bienvenidos-----------\n");
+            Console.WriteLine("Cuantos quieren jugar hoy: 2, 3 o 4 ");
+            numJugadores = Convert.ToInt32(Console.ReadLine());
+            
+            if (numJugadores ==2)
+            {
+
+            }
+
         }
     }
 }
