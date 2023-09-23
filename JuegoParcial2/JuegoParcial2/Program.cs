4385cab254f24e100b727ae45ae0db161e593575
SANTIAGOGALVIS04 <sgalvis1904@gmail.com>
2023-09-22 21:27:49 -0500

    Metemos todo dentro de un do while para verificar si desean continuar y limpiar la consola

--- JuegoParcial2/JuegoParcial2/Program.cs
+++ JuegoParcial2/JuegoParcial2/Program.cs
@@ -8,75 +8,89 @@ namespace JuegoParcial2
         {
             //creación de un juego
             // declaración de variables
-
-            Random numeroRandom = new Random();
-
             int numeroJugadores, numeroEscogido, numeroAleatorio, jugador = 1;
+            String jugarDeNuevo;
             bool volverJugar = true;
 
-      
+            Random numeroRandom = new Random();
+            
+            
+            do {
 
-            //Proceso del juego
+                Console.Clear();
 
-            //Pedimos el número de jugadores
+                //Proceso del juego
 
-            Console.WriteLine("Bienvenidos al juego de adivinar el número");
-            Console.WriteLine("Ingrese el número de jugadores que desean jugar\n" +
-                "mínimo deben de ser 2 jugadores y máximo 4");
+                //Pedimos el número de jugadores
 
-            numeroJugadores = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Bienvenidos al juego de adivinar el número");
+                Console.WriteLine("Ingrese el número de jugadores que desean jugar\n" +
+                    "mínimo deben de ser 2 jugadores y máximo 4");
 
-            numeroAleatorio = numeroRandom.Next(0,200);
-
-            if (numeroJugadores == 2)
-            {
-
-                numeroAleatorio = numeroRandom.Next(0, 50);
-
-            } else if(numeroJugadores == 3){
-
-                numeroAleatorio = numeroRandom.Next(0, 100);
-
-            } else if (numeroJugadores == 4){
+                numeroJugadores = Convert.ToInt32(Console.ReadLine());
 
                 numeroAleatorio = numeroRandom.Next(0, 200);
 
-            } else{
-
-                Console.WriteLine("Ingreso un número de jugadores no aceptado");
-
-            }
-
-
-            Console.WriteLine("La maquina ha escogido el número");
-
-            for (int i = 1; i <= numeroJugadores; i++)
-            {
-
-                Console.WriteLine($"Jugador numero {jugador} ingrese su número");
-                jugador++;
-                numeroEscogido = Convert.ToInt32(Console.ReadLine());
-
-                if (numeroEscogido < numeroAleatorio)
+                if (numeroJugadores == 2)
                 {
 
-                    Console.WriteLine("ES MAYOR");
+                    numeroAleatorio = numeroRandom.Next(0, 50);
 
-                }else if (numeroEscogido > numeroAleatorio) {
+                } else if (numeroJugadores == 3) {
 
-                    Console.WriteLine("ES MENOR");
+                    numeroAleatorio = numeroRandom.Next(0, 100);
 
-                }
-                else
-                {
-                    Console.WriteLine("¡HAS GANADO!");
-                   
+                } else if (numeroJugadores == 4) {
+
+                    numeroAleatorio = numeroRandom.Next(0, 200);
+
+                } else {
+
+                    Console.WriteLine("Ingreso un número de jugadores no aceptado");
+
                 }
 
 
+                Console.WriteLine("La maquina ha escogido el número");
 
-            }
-            
+                for (int i = 1; i <= numeroJugadores; i++)
+                {
+
+                    Console.WriteLine($"Jugador numero {jugador} ingrese su número");
+                    jugador++;
+                    numeroEscogido = Convert.ToInt32(Console.ReadLine());
+
+                    if (numeroEscogido < numeroAleatorio)
+                    {
+
+                        Console.WriteLine("ES MAYOR");
+
+                    } else if (numeroEscogido > numeroAleatorio) {
+
+                        Console.WriteLine("ES MENOR");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("¡HAS GANADO!");
+
+                    }
+
+
+
+                }
+
+                Console.WriteLine("¿Desea seguir jugando? (S/N)");
+                jugarDeNuevo = Convert.ToString(Console.ReadLine());
+
+                if (jugarDeNuevo != "S")
+                {
+                    volverJugar = false;
+                }
+
+                
+
+            } while (volverJugar =  true);
 
 
         }
