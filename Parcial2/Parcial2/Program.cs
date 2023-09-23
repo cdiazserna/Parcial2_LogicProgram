1208a021550d449eeb651e712e1bb309c1859f62
SantiagoOspinaR <ospinasantiago41@gmail.com>
2023-09-22 20:17:43 -0500

    Declaración de variables del juego

---
+++ Parcial2/Parcial2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Desarrollar el juego “Adivina el número” en un programa de C#.
+//El juego es muy sencillo, consiste en adivinar  un  número N aleatorio  que  el  sistema  escoge  de  un  determinado  rango  y
+//lo guarda  en memoria sin ser revelado.
+public class JuegoParcial2
+{
+    static void Main(string[] args)
+{
+        Console.WriteLine("----------ADIVINA EL NÚMERO----------");
+
+        //Declaracion de las variables
+        int numeroJugadores, numeroGanador;
+        bool proximoIntento = true;
+
+
+
+}
+
+
+}
