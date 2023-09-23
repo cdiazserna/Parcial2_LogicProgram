b49913c23184f8158399b439e3c7b2ce380c6abf
Juan Manuel Ospina Londoño <juanmasgr@gmail.com>
2023-09-22 20:52:23 -0500

    en èste primer paso lo que hago es declarar algunas variables ya que a medida que se redacta el còdigo voy anexando las que se necesiten, tambièn pongo algunos mensajes de validaciòn

--- JuegoParcial2/Program.cs
+++ JuegoParcial2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace juegoparcial2
+
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int numjugadores, num1;
+            const string MESSAGE = "Por favor ingresar un número de jugadores";
+            
+
+            Console.Write("Ingrese un numero entero del 1 al 100 ");
+            if (!int.TryParse(Console.ReadLine(), out num1)) 
+            do
+            {
+                Console.WriteLine(MESSAGE);
+                Console.Clear();
+                Console.Write("Ingrese el primer número: ");
+                Random r = new Random();
+
+         
+            Console.WriteLine(r.Next(1, 101));
+
+            Console.ReadLine();
+        }
+
+    }
+}
