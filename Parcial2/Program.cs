f934436e703abf536a6bf041e67bbc9b24eeb0ca
omora79 <oamm30@hotmail.com>
2023-09-22 20:30:02 -0500

    revicion

--- Parcial2/Program.cs
+++ Parcial2/Program.cs
@@ -19,7 +19,30 @@ namespace Parcial2
             
             if (numJugadores ==2)
             {
+                n = numrandom.Next(0, 50);
+                for (i = 0, i = 50; i++)
+                {
+                    Console.WriteLine("primer jugador ingresa tu numero entre 0 y 50:  ");
+                    player1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("segundo jugador ingresa tu numero entre 0 y 50:  ");
+                    player2 = Convert.ToInt32(Console.ReadLine());
+                    if (player1 == n)
+                    {
+                        Console.WriteLine("el primer jugador a ganado");
 
+                    }
+                    else if (player1 < n)
+                    {
+                        Console.WriteLine("el numero es mayor");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero es menor");
+                    }
+                    }
+                }
+                
+                
             }
 
         }
