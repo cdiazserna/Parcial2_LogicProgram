5933c3c0f673aaf4e14ecb2c91d8343583f71cd2
omora79 <oamm30@hotmail.com>
2023-09-22 21:14:31 -0500

    entrada del while

--- Parcial2/Program.cs
+++ Parcial2/Program.cs
@@ -9,6 +9,7 @@ namespace Parcial2
             //definir las varibles
 
             int numJugadores, player1, player2, player3, player4, i, n;
+            bool repetir = true;
             Random numrandom = new Random();
 
             // pedir datos a los participantes.
@@ -108,10 +109,70 @@ namespace Parcial2
 
 
             }
-            else(numJugadores == 4)
+            else if(numJugadores == 4)
+            {
+                n = numrandom.Next(0, 200);
+                for (i = 0; i <= 200; i++)
+                {
+                    Console.WriteLine("primer jugador ingresa tu numero entre 0 y 200:  ");
+                    player1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("segundo jugador ingresa tu numero entre 0 y 200:  ");
+                    player2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("tercer jugador ingresa tu numero entre 0 y 200:  ");
+                    player3 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("cuarto jugador ingresa tu numero entre 0 y 200:  ");
+                    player4 = Convert.ToInt32(Console.ReadLine());
+                    if (player1 == n)
+                    {
+                        Console.WriteLine("el primer jugador a ganado");
 
-            
-
+                    }
+                    else if (player1 < n)
+                    {
+                        Console.WriteLine("el numero del primer jugador es mayor");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero del primer jugador es  menor");
+                    }
+                    if (player2 == n)
+                    {
+                        Console.WriteLine("el segundo jugador a ganado");
+                    }
+                    else if (player2 < n)
+                    {
+                        Console.WriteLine("el numero del segundo jugador es mayor");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero del segundo jugador es  menor");
+                    }
+                    if (player3 == n)
+                    {
+                        Console.WriteLine("el tercer jugador a ganado");
+                    }
+                    else if (player3 < n)
+                    {
+                        Console.WriteLine("el numero del tercer jugador es mayor");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero del tercer jugador es  menor");
+                    }
+                    if (player4 == n)
+                    {
+                        Console.WriteLine("el cuarto jugador a ganado");
+                    }
+                    else if (player4 < n)
+                    {
+                        Console.WriteLine("el numero del cuarto jugador es mayor");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero del cuarto jugador es  menor");
+                    }
+                }
+            }
 
         }
     }
